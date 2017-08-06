75739b46481eb4a3ec51244076e0413e79eb2ec1
XieLong <1293336945@qq.com>
2017-08-06 13:27:43 +0800

    update

--- Test1/widget.cpp
+++ Test1/widget.cpp
@@ -1,4 +1,5 @@
 #include "widget.h"
+#include <QMessageBox>
 #include "ui_widget.h"
 
 Widget::Widget(QWidget *parent) :
@@ -11,4 +12,21 @@ Widget::Widget(QWidget *parent) :
 Widget::~Widget()
 {
   delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    //QMessageBox::information(this, tr("O的K"), tr("我觉得不行"));
+//  QMessageBox *msg = new QMessageBox(this);
+//  msg->setWindowTitle("Alert");
+//  msg->setText("我觉得还行");
+//  msg->setBaseSize(320, 240);
+//  msg->setButtonText(1, tr("不行"));
+//  msg->exec();
+  QMessageBox msg(this);
+  msg.setWindowTitle("Alert");
+  msg.setText("我觉得还行");
+  msg.setBaseSize(320,480);
+  msg.setButtonText(1,"不行");
+  msg.exec();
 }
