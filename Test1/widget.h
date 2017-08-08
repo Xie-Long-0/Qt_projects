ccdb9229d6dd6c67f658cae249f5a2f73233b4ea
XieLong <1293336945@qq.com>
2017-08-08 14:55:58 +0800

    upgrade to radioButton section

--- Test1/widget.h
+++ Test1/widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QButtonGroup>    //按钮分组类
 
 namespace Ui {
   class Widget;
@@ -15,10 +16,21 @@ public:
   explicit Widget(QWidget *parent = 0);
   ~Widget();
 
+public slots:
+  void recvGenderID(int id);    //接收性别单选按钮分组信号
+  void recvStatusID(int id);    //接收状态单选按钮分组信号
+
 private slots:
-  void on_pushButton_clicked();
+  void on_radBtn_gpBox_1_toggled(bool checked);
+  void on_radBtn_gpBox_2_toggled(bool checked);
+  void on_radBtn_gpBox_3_toggled(bool checked);
+
+  void on_pushButton_clicked();    //点击按钮
 
 private:
+  QButtonGroup *m_pGenderGroup;    //性别单选按钮分组
+  QButtonGroup *m_pStatusGroup;    //状态单选按钮分组
+
   Ui::Widget *ui;
 };
 
