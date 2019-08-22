b5982d2e70886b61e100e29982f032236d5d5558
XieLong <1293336945@qq.com>
2019-08-22 17:45:18 +0800

    done

--- exp4_button/widget.cpp
+++ exp4_button/widget.cpp
@@ -3,6 +3,8 @@
 
 #include <QDebug>
 #include <QMessageBox>
+#include <QUrl>
+#include <QDesktopServices>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -25,6 +27,12 @@ Widget::Widget(QWidget *parent) :
             this, &Widget::recvRolesID);
     connect(pRadioGenderGroup, static_cast<void(QButtonGroup::*)(int)>(&QButtonGroup::buttonClicked),
             this, &Widget::recvGenderID);
+
+    //设置三态复选框
+    ui->checkBoxGame->setTristate(true);
+    ui->checkBoxMusic->setTristate(true);
+    ui->checkBoxComic->setTristate(true);
+    ui->checkBoxProgram->setTristate(true);
 }
 
 Widget::~Widget()
@@ -32,17 +40,97 @@ Widget::~Widget()
     delete ui;
 }
 
+int Widget::checkGames() const
+{
+    int count = 0;
+    if (ui->checkBoxGameArknights->isChecked())
+        count++;
+    if (ui->checkBoxGameFate->isChecked())
+        count++;
+    if (ui->checkBoxGameTouhou->isChecked())
+        count++;
+
+    if (count <= 0)
+        ui->checkBoxGame->setCheckState(Qt::Unchecked);
+    else if (count >= 3)
+        ui->checkBoxGame->setCheckState(Qt::Checked);
+    else
+        ui->checkBoxGame->setCheckState(Qt::PartiallyChecked);
+
+    return count;
+}
+
+int Widget::checkMusics() const
+{
+    int count = 0;
+    if (ui->checkBoxMusicJayChou->isChecked())
+        count++;
+    if (ui->checkBoxMusicTouhou->isChecked())
+        count++;
+    if (ui->checkBoxMusicVae->isChecked())
+        count++;
+
+    if (count <= 0)
+        ui->checkBoxMusic->setCheckState(Qt::Unchecked);
+    else if (count >= 3)
+        ui->checkBoxMusic->setCheckState(Qt::Checked);
+    else
+        ui->checkBoxMusic->setCheckState(Qt::PartiallyChecked);
+
+    return count;
+}
+
+int Widget::checkComics() const
+{
+    int count = 0;
+    if (ui->checkBoxComicConan->isChecked())
+        count++;
+    if (ui->checkBoxComicFate->isChecked())
+        count++;
+    if (ui->checkBoxComicKaguya->isChecked())
+        count++;
+
+    if (count <= 0)
+        ui->checkBoxComic->setCheckState(Qt::Unchecked);
+    else if (count >= 3)
+        ui->checkBoxComic->setCheckState(Qt::Checked);
+    else
+        ui->checkBoxComic->setCheckState(Qt::PartiallyChecked);
+
+    return count;
+}
+
+int Widget::checkPrograms() const
+{
+    int count = 0;
+    if (ui->checkBoxProgramC->isChecked())
+        count++;
+    if (ui->checkBoxProgramJava->isChecked())
+        count++;
+    if (ui->checkBoxProgramPython->isChecked())
+        count++;
+
+    if (count <= 0)
+        ui->checkBoxProgram->setCheckState(Qt::Unchecked);
+    else if (count >= 3)
+        ui->checkBoxProgram->setCheckState(Qt::Checked);
+    else
+        ui->checkBoxProgram->setCheckState(Qt::PartiallyChecked);
+
+    return count;
+}
+
 void Widget::recvRolesID(int id)
 {
     switch (id) {
     case 0:
-        qDebug() << tr("妈死塔");
+        qDebug() << tr("骂死塔");
         break;
     case 1:
         qDebug() << tr("刀客塔");
         break;
     case 2:
-        qDebug() << tr("硕博连读");
+        qDebug() << tr("Kono DIO da!");
         break;
     default:
         break;
@@ -99,6 +187,7 @@ void Widget::on_radioButton_4_toggled(bool checked)
         qDebug() << tr("不是咸鱼玩家");
 }
 
+//综合显示
 void Widget::on_pushButton_clicked()
 {
     QString strResult = "";
@@ -135,21 +224,219 @@ void Widget::on_pushButton_clicked()
         break;
     }
 
-    if (ui->radioButton->isChecked()) {
+    if (ui->radioButton->isChecked())
         strResult += tr("类别：无氪玩家\n");
-    }
-    else if (ui->radioButton_2->isChecked()) {
+    else if (ui->radioButton_2->isChecked())
         strResult += tr("类别：非洲玩家\n");
-    }
-    else if (ui->radioButton_3->isChecked()) {
+    else if (ui->radioButton_3->isChecked())
         strResult += tr("类别：欧洲玩家\n");
-    }
-    else if (ui->radioButton_4->isChecked()) {
+    else if (ui->radioButton_4->isChecked())
         strResult += tr("类别：咸鱼玩家\n");
-    }
-    else {
+    else
         strResult += tr("类别：未选中\n");
+
+    strResult += tr("喜欢的东西：");
+    int count = 0;
+    if (ui->checkBoxGame->checkState() > 0) {
+        strResult += tr("游戏 ");
+        count++;
     }
+    if (ui->checkBoxMusic->checkState() > 0) {
+        strResult += tr("音乐 ");
+        count++;
+    }
+    if (ui->checkBoxComic->checkState() > 0) {
+        strResult += tr("动漫 ");
+        count++;
+    }
+    if (ui->checkBoxProgram->checkState() > 0) {
+        strResult += tr("编程");
+        count++;
+    }
+    if (0 == count)
+        strResult += tr("未选中");
 
     QMessageBox::information(this, tr("综合信息"), strResult);
+}
+
+void Widget::on_checkBoxGameFate_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("Fate系列游戏");
+    checkGames();
+}
+
+void Widget::on_checkBoxGameTouhou_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("东方系列游戏");
+    checkGames();
+}
+
+void Widget::on_checkBoxGameArknights_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("明日方舟");
+    checkGames();
+}
+
+void Widget::on_checkBoxMusicTouhou_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("东方系列音乐");
+    checkMusics();
+}
+
+void Widget::on_checkBoxMusicVae_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("许嵩的歌曲");
+    checkMusics();
+}
+
+void Widget::on_checkBoxMusicJayChou_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("周董的歌曲");
+    checkMusics();
+}
+
+void Widget::on_checkBoxComicFate_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("Fate系列动漫");
+    checkComics();
+}
+
+void Widget::on_checkBoxComicKaguya_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("辉夜大小姐想让我告白～");
+    checkComics();
+}
+
+void Widget::on_checkBoxComicConan_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("名侦探柯南");
+    checkComics();
+}
+
+void Widget::on_checkBoxProgramC_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("C/C++");
+    checkPrograms();
+}
+
+void Widget::on_checkBoxProgramJava_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("Java");
+    checkPrograms();
+}
+
+void Widget::on_checkBoxProgramPython_toggled(bool checked)
+{
+    if (checked)
+        qDebug() << tr("Python");
+    checkPrograms();
+}
+
+void Widget::on_cmdLinkButtonHome_clicked()
+{
+    QDesktopServices::openUrl(QUrl("file:///home/xielong"));
+}
+
+void Widget::on_cmdLinkButtonGoogle_clicked()
+{
+    QDesktopServices::openUrl(QUrl("https://google.com"));
+}
+
+void Widget::on_checkBoxGame_stateChanged(int arg1)
+{
+    switch (arg1) {
+    case 0:
+        ui->checkBoxGameArknights->setChecked(false);
+        ui->checkBoxGameFate->setChecked(false);
+        ui->checkBoxGameTouhou->setChecked(false);
+        break;
+    case 1:
+        if (checkGames() <= 0)
+            ui->checkBoxGameFate->setChecked(true);
+        break;
+    case 2:
+        ui->checkBoxGameArknights->setChecked(true);
+        ui->checkBoxGameFate->setChecked(true);
+        ui->checkBoxGameTouhou->setChecked(true);
+        break;
+    default:
+        break;
+    }
+}
+
+void Widget::on_checkBoxMusic_stateChanged(int arg1)
+{
+    switch (arg1) {
+    case 0:
+        ui->checkBoxMusicJayChou->setChecked(false);
+        ui->checkBoxMusicTouhou->setChecked(false);
+        ui->checkBoxMusicVae->setChecked(false);
+        break;
+    case 1:
+        if (checkMusics() <= 0)
+            ui->checkBoxMusicTouhou->setChecked(true);
+        break;
+    case 2:
+        ui->checkBoxMusicJayChou->setChecked(true);
+        ui->checkBoxMusicTouhou->setChecked(true);
+        ui->checkBoxMusicVae->setChecked(true);
+        break;
+    default:
+        break;
+    }
+}
+
+void Widget::on_checkBoxComic_stateChanged(int arg1)
+{
+    switch (arg1) {
+    case 0:
+        ui->checkBoxComicConan->setChecked(false);
+        ui->checkBoxComicFate->setChecked(false);
+        ui->checkBoxComicKaguya->setChecked(false);
+        break;
+    case 1:
+        if (checkComics() <= 0)
+            ui->checkBoxComicFate->setChecked(true);
+        break;
+    case 2:
+        ui->checkBoxComicConan->setChecked(true);
+        ui->checkBoxComicFate->setChecked(true);
+        ui->checkBoxComicKaguya->setChecked(true);
+        break;
+    default:
+        break;
+    }
+}
+
+void Widget::on_checkBoxProgram_stateChanged(int arg1)
+{
+    switch (arg1) {
+    case 0:
+        ui->checkBoxProgramC->setChecked(false);
+        ui->checkBoxProgramJava->setChecked(false);
+        ui->checkBoxProgramPython->setChecked(false);
+        break;
+    case 1:
+        if (checkPrograms() <= 0)
+            ui->checkBoxProgramC->setChecked(true);
+        break;
+    case 2:
+        ui->checkBoxProgramC->setChecked(true);
+        ui->checkBoxProgramJava->setChecked(true);
+        ui->checkBoxProgramPython->setChecked(true);
+        break;
+    default:
+        break;
+    }
 }
--- exp4_button/widget.h
+++ exp4_button/widget.h
@@ -16,6 +16,12 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
+    //检查三态复选框
+    int checkGames() const;
+    int checkMusics() const;
+    int checkComics() const;
+    int checkPrograms() const;
+
 public slots:
     void recvRolesID(int id);
     void recvGenderID(int id);
@@ -27,6 +33,27 @@ private slots:
     void on_radioButton_4_toggled(bool checked);
 
     void on_pushButton_clicked();
+
+    void on_checkBoxGameFate_toggled(bool checked);
+    void on_checkBoxGameTouhou_toggled(bool checked);
+    void on_checkBoxGameArknights_toggled(bool checked);
+    void on_checkBoxMusicTouhou_toggled(bool checked);
+    void on_checkBoxMusicVae_toggled(bool checked);
+    void on_checkBoxMusicJayChou_toggled(bool checked);
+    void on_checkBoxComicFate_toggled(bool checked);
+    void on_checkBoxComicKaguya_toggled(bool checked);
+    void on_checkBoxComicConan_toggled(bool checked);
+    void on_checkBoxProgramC_toggled(bool checked);
+    void on_checkBoxProgramJava_toggled(bool checked);
+    void on_checkBoxProgramPython_toggled(bool checked);
+
+    void on_cmdLinkButtonHome_clicked();
+    void on_cmdLinkButtonGoogle_clicked();
+
+    void on_checkBoxGame_stateChanged(int arg1);
+    void on_checkBoxMusic_stateChanged(int arg1);
+    void on_checkBoxComic_stateChanged(int arg1);
+    void on_checkBoxProgram_stateChanged(int arg1);
 
 private:
     Ui::Widget *ui;
