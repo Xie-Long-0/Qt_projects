ed2c73b6ce353fd6c3556e4011e1e94b01abd876
XieLong <1293336945@qq.com>
2019-09-08 23:35:15 +0800

    add exp8

---
+++ exp8_ImageShow/widget.h
@@ -0,0 +1,43 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QPixmap>
+#include <QMovie>
+#include <QImageReader>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+public slots:
+    void recvPlayError(QImageReader::ImageReaderError error);   //接收播放错误信号
+    void recvFrameNumber(int frameNumber);          //接收帧号变化信号
+
+private slots:
+    void on_pushButton_OpenImage_clicked();
+    void on_pushButton_OpenMovie_clicked();
+    void on_pushButton_Func1_clicked();
+    void on_pushButton_Func2_clicked();
+    void on_pushButton_Func3_clicked();
+
+private:
+    Ui::Widget *ui;
+
+    QPixmap *m_pPixMap;
+    QMovie *m_pMovie;
+    bool m_isMovie;
+    bool m_isPlaying;
+
+    void clearOld();
+};
+
+#endif // WIDGET_H
