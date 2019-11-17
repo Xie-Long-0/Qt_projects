f0d9098b82a2e2f86d391686a86fefaa973fb8c8
XieLong <1293336945@qq.com>
2019-11-17 12:19:42 +0800

    Add icon and splash

--- exp8_ImageShow/main.cpp
+++ exp8_ImageShow/main.cpp
@@ -1,11 +1,34 @@
 #include "widget.h"
 #include <QApplication>
 
+#include <QPixmap>
+#include <QSplashScreen>
+#include <QThread>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     Widget w;
+    QPixmap pixSplash(":/resource/splash.jpg");
+    pixSplash = pixSplash.scaled(w.size());
+
+    //创建闪屏
+    QSplashScreen splash(pixSplash);
+    splash.setGeometry(w.geometry());
+    //显示闪屏和提示信息
+    splash.show();      //NOTE: 本人在 qt5.9.5 on Ubuntu 18.04下运行时splash只显示一个透明矩形，
+                        //      并在最后一瞬才显示图片，具体原因暂不明。
+    splash.showMessage(QObject::tr("先看一下老婆～"), Qt::AlignBottom|Qt::AlignHCenter);
+
+    //启动应用程序的事件处理
+    a.processEvents();
+
+    w.thread()->sleep(3);
     w.show();
+
+    //主窗体显示后结束闪屏
+    splash.finish(&w);
 
     return a.exec();
 }
